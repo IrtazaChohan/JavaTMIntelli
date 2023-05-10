ee3f3e84cbb884e2356e6580345c987ea34f84b3
Irtaza Chohan <irtazachohan@gmail.com>
2023-05-10 12:50:15 +0100

    HelloWorld Initial - fixed

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) { //psvm is keyboard shortcut
-        System.out.println("HelloWorld"); //sout is keyboard shortcut
+        System.out.println("Hello World"); //sout is keyboard shortcut
     }
 }
