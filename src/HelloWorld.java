3fd0677baaf79e9ffa9a8409e8982e23aff18d7f
Irtaza Chohan <irtazachohan@gmail.com>
2023-05-10 16:28:46 +0100

    person Class Constructor

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) { //psvm is keyboard shortcut
-        System.out.println("Hello World"); //sout is keyboard shortcut
+        System.out.println("Hello  - Trying out the course even though this is basic!"); //sout is keyboard shortcut
     }
 }
---
+++ src/model/person.java
@@ -0,0 +1,39 @@
+package model;
+
+import java.lang.reflect.Constructor;
+import java.time.LocalDate;
+public class person {
+    private String firstName;
+    private String lastName;
+    private LocalDate dob;
+
+    public person(String firstName, String lastName, LocalDate dob) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = dob;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+}
